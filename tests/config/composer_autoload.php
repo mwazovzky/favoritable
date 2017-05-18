e92644eff48fc5dc66609da25c44cef597ea224b
Mike <mike.wazovzky@gmail.com>
2017-05-18 09:59:46 +0300

    Updated test environment for Travis (composer_autoload.php)

--- tests/config/composer_autoload.php
+++ tests/config/composer_autoload.php
@@ -4,8 +4,8 @@ try {
 	$myFile = json_decode(file_get_contents('composer.json'), true);
 
 	$myFile['autoload']['psr-4']["Mikewazovzky\\Favoritable\\"] = 'vendor/Mikewazovzky/Favoritable/src/';
-	$myFile['autoload-dev']['classmap'] = [ 'vendor/Mikewazovzky/Favoritable/tests' ];
-	$myFile['autoload-dev']['files'] = [ 'tests/functions.php' ];
+	$myFile['autoload-dev']['files'] = [ 'tests/functions.php' ];	
+	$myFile['autoload-dev']['classmap'] = [ 'vendor/mikewazovzky/favoritable/tests' ];
 	
 	file_put_contents('composer.json', json_encode($myFile, JSON_PRETTY_PRINT)); // JSON_UNESCAPED_SLASHES));
 
