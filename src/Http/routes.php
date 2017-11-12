d49c8bcbb728554496b279e29657065ad0ebdf98
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-11-12 11:11:58 +0300

    Route group namespace

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,10 +1,10 @@
 <?php
 
-Route::group(['middleware' => ['web']], function () {
-
-    Route::post('/favorites/{model}/{id}', 'Mikewazovzky\Favoritable\Http\FavoritesController@store')
-        ->name('favorites.store');
-
-    Route::delete('/favorites/{model}/{id}', 'Mikewazovzky\Favoritable\Http\FavoritesController@destroy')
-        ->name('favorites.destroy');
-});
+Route::middleware('web')
+    ->namespace('Mikewazovzky\Favoritable\Http')
+    ->group(function () {
+        Route::post('/favorites/{model}/{id}', 'FavoritesController@store')
+            ->name('favorites.store');
+        Route::delete('/favorites/{model}/{id}', 'FavoritesController@destroy')
+            ->name('favorites.destroy');
+    });
