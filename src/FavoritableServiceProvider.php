94d97589fd857cadae25e1fe62d7623de67fd4c0
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-11-12 11:04:26 +0300

    Error fixed

--- src/FavoritableServiceProvider.php
+++ src/FavoritableServiceProvider.php
@@ -6,30 +6,28 @@ use Illuminate\Support\ServiceProvider;
 
 class FavoritableServiceProvider extends ServiceProvider
 {
-
     public function boot()
     {
         // load and make available to the application package routes
         $this->loadRoutesFrom(__DIR__ . '/Http/routes.php');
 
         // load and make available to the application package migrations
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
-        //  load and make available to the application package views
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'favoritable');
+        // load and make available to the application package views
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'favoritable');
 
         // Allow application to publish and modify package assets grouped by asset type tag
         // php artisan vendor:publish --tag=sometag --force
-
         // --tag=migrations
         // $this->publishes([
         //     __DIR__ . '/../database/migrations' => $this->app->databasePath() . '/migrations'
         // ], 'migrations');
 
         // --tag=views
-        $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/favoritable'),
-        ], 'views');
+        // $this->publishes([
+        //     __DIR__.'/../resources/views' => resource_path('views/vendor/favoritable'),
+        // ], 'views');
 
         // --tag=assets
         $this->publishes([
